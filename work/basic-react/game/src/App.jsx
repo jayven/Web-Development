35b80e33f619050613d5bf2da2274c4e7d35ec2c
Jayven <yin.jiaw@husky.neu.edu>
2021-04-03 16:42:23 -0700

    Assignment

---
+++ work/basic-react/game/src/App.jsx
@@ -0,0 +1,17 @@
+import Game from './Game'
+import './App.css';
+import './style.css';
+
+function App() {
+
+  return (
+    <div className="app">
+      <div className="title">
+      	<h2>Welcome To Guess Word Game</h2>
+      </div>
+      <Game/>
+    </div>
+  );
+}
+
+export default App;
